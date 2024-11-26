67cdec17cdf00fa908180bb42965beef4eacc568
couldBeFree <zoriaalexandr@gmail.com>
2024-11-26 18:39:39 +0200

    fixed date format

--- src/utils/getCurrentISODate.ts
+++ src/utils/getCurrentISODate.ts
@@ -1,7 +1,7 @@
 const getCurrentISODate = () => {
   const today = new Date();
   today.setHours(12, 30, 0, 0);
-  return today.toISOString();
+  return today;
 };
 
 export default getCurrentISODate;
