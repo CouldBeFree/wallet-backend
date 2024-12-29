efeed126b4b88f9af0308db24adcd550e7754486
couldBeFree <zoriaalexandr@gmail.com>
2024-12-29 12:46:31 +0200

    configure mongo from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import { AuthModule } from './auth/auth.module';
 import { ExpensesModule } from './modules/expenses/expenses.module';
 import { IncomesModule } from './modules/incomes/incomes.module';
 import { StatisticModule } from './modules/statistic/statistic.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/wallet_watch'),
+    MongooseModule.forRoot(
+      process.env.MONGO_DB_URL || 'mongodb://localhost:27017/wallet_watch',
+    ),
     AuthModule,
     ExpensesModule,
     IncomesModule,
