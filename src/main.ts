a069c06f4b5079e3c69a5ad076bc3cbf88a64a0a
couldBeFree <zoriaalexandr@gmail.com>
2024-12-29 13:08:26 +0200

    configured cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://wallet-backend-production-54e7.up.railway.app'],
+    origin: [process.env.CLIENT_URL],
   });
   await app.listen(3000);
 }
+
 bootstrap();
