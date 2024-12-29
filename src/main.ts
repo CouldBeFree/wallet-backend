29bc16838d1cc13358e6781f73566d7b9a1190d4
couldBeFree <zoriaalexandr@gmail.com>
2024-12-29 12:58:17 +0200

    configured cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://wallet-backend-production-54e7.up.railway.app'],
+  });
   await app.listen(3000);
 }
 bootstrap();
